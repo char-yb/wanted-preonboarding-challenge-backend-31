a6350d98f9027ef3a0bea7e3c7b0b334428e2659
ybchar <uiurihappy@naver.com>
2025-04-30 13:39:10 +0900

    chore: Apply gradle catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,19 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.4.5"
-	id("io.spring.dependency-management") version "1.1.7"
-	kotlin("plugin.jpa") version "1.9.25"
+	alias(libs.plugins.kotlin.jvm)
+	alias(libs.plugins.kotlin.kapt)
+	alias(libs.plugins.kotlin.spring)
+	alias(libs.plugins.kotlin.jpa)
+	alias(libs.plugins.spring.boot)
+	alias(libs.plugins.spring.dependency.management)
+	alias(libs.plugins.ktlint)
 }
 
-group = "com.commerce"
-version = "0.0.1-SNAPSHOT"
+ktlint {
+	version.set(libs.versions.ktlint.version.set)
+}
+
+version = project.findProperty("applicationVersion")?.toString() ?: "0.0.1-SNAPSHOT"
+group = project.findProperty("projectGroup")?.toString() ?: "com.commerce"
 
 java {
 	toolchain {
@@ -22,23 +28,39 @@ repositories {
 extra["springModulithVersion"] = "1.3.5"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.springframework.modulith:spring-modulith-starter-core")
-	implementation("org.springframework.modulith:spring-modulith-starter-jpa")
-	runtimeOnly("com.h2database:h2")
-	runtimeOnly("org.postgresql:postgresql")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testImplementation("org.springframework.modulith:spring-modulith-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	// Spring
+	implementation(libs.spring.boot.starter.web)
+	implementation(libs.spring.boot.starter.data.jpa)
+	implementation(libs.spring.boot.starter.data.jdbc)
+	implementation(libs.spring.boot.starter.actuator)
+
+	// Libraries
+	implementation(libs.jackson.kotlin)
+	implementation(libs.kotlin.reflect)
+
+	// Spring Modulith (bundle 사용)
+	implementation(libs.bundles.spring.modulith)
+	runtimeOnly(libs.bundles.spring.modulith.runtime)
+
+	// Database
+	runtimeOnly(libs.postgresql)
+	runtimeOnly(libs.h2)
+
+	// Test
+	testImplementation(libs.spring.boot.starter.test)
+	testImplementation(libs.kotlin.test.junit5)
+	testRuntimeOnly(libs.junit.platform)
+
+	testImplementation(libs.spring.modulith.test)
 }
 
 dependencyManagement {
 	imports {
-		mavenBom("org.springframework.modulith:spring-modulith-bom:${property("springModulithVersion")}")
+		mavenBom(
+			libs.spring.modulith.bom
+				.get()
+				.toString(),
+		)
 	}
 }
 
@@ -46,6 +68,23 @@ kotlin {
 	compilerOptions {
 		freeCompilerArgs.addAll("-Xjsr305=strict")
 	}
+}
+
+tasks.register("addLintPreCommitHook", DefaultTask::class) {
+	group = "setup"
+	description = "Install git hooks"
+	doLast {
+		val hooksDir = project.file(".git/hooks")
+		val scriptDir = project.file("scripts")
+		val preCommit = scriptDir.resolve("pre-commit")
+		Runtime.getRuntime().exec("chmod +x .git/hooks/pre-commit")
+		preCommit.copyTo(hooksDir.resolve("pre-commit"), overwrite = true)
+		hooksDir.resolve("pre-commit").setExecutable(true)
+	}
+}
+
+tasks.named("compileKotlin") {
+	dependsOn("addLintPreCommitHook")
 }
 
 allOpen {
